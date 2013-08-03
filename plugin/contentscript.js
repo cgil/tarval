ba1ad8271392adc2058bd0f0a6b4c1ce5b4f716d
Benjamin Vishny <benjamin.vishny@10gen.com>
2013-08-03 16:58:16 -0400

    Add addScript and JQ

--- plugin/contentscript.js
+++ plugin/contentscript.js
@@ -7,8 +7,17 @@ function exec(fn) {
    document.documentElement.removeChild(script); // clean up
 }
 
+function addScript(uri) {
+    var scr = document.createElement('script');
+
+    scr.setAttribute('src', uri);
+
+    document.head.appendChild(scr);
+}
+
 script = function() {
   test = "hello";
 }
 
 exec(script);
+addScript("http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js");
